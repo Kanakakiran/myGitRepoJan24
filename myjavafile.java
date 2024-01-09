236dedc425df035535fb3447e8f8ab60cff2d08c
Kanakakiran <kanakakiran84@gmail.com>
2024-01-09 22:50:24 +0530

    my task completed

--- myjavafile.java
+++ myjavafile.java
@@ -1,1 +1,10 @@
+<<<<<<< Updated upstream
 This is my manager work completed
+=======
+this is my java file
+1st day of java line of code completed
+2nd day of java line of code completed
+3rd day of java line of code completed
+4th day of java line of code completed
+5th day of java line of code completed
+>>>>>>> Stashed changes
